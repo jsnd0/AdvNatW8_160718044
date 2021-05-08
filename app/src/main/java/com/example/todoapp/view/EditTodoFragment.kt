031b5c2286f5e225da37cd27d94487494a03d336
Zero <jason.daryolla2@gmail.com>
2021-05-08 18:34:43 +0700

    bug fixes

--- app/src/main/java/com/example/todoapp/view/EditTodoFragment.kt
+++ app/src/main/java/com/example/todoapp/view/EditTodoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.todoapp.R
@@ -24,7 +25,8 @@ class EditTodoFragment : Fragment(), RadioClick, TodoSaveChangesClick {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_todo, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_edit_todo, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -31,16 +31,17 @@ class TodoListAdapter (val todoList:ArrayList<Todo>, val adapterOnClick : (Any) 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.view.todo = todoList[position]
         holder.view.listener=this
-        holder.view.checkTask.setText(todoList[position].title.toString())
+        holder.view.editListener=this
+//        holder.view.checkTask.setText(todoList[position].title.toString())
 
-        holder.view.imgEdit.setOnClickListener {
-            val action = TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
-            Navigation.findNavController(it).navigate(action)
-        }
+//        holder.view.imgEdit.setOnClickListener {
+//            val action = TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
+//            Navigation.findNavController(it).navigate(action)
+//        }
 
-        holder.view.checkTask.setOnCheckedChangeListener { compoundButton, isChecked ->
-            adapterOnClick(todoList[position])
-        }
+//        holder.view.checkTask.setOnCheckedChangeListener { compoundButton, isChecked ->
+//            adapterOnClick(todoList[position])
+//        }
 
     }
 

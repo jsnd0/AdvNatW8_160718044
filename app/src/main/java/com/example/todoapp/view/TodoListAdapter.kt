0fa5793fb952d365cfb5e6d896227fee7ab16957
Zero <jason.daryolla2@gmail.com>
2021-05-08 17:19:04 +0700

    finished attribute binding

--- app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -3,34 +3,37 @@ package com.example.todoapp.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
+import com.example.todoapp.databinding.TodoItemLayoutBinding
 import com.example.todoapp.model.Todo
 import kotlinx.android.synthetic.main.todo_item_layout.view.*
+import java.util.zip.Inflater
 
 class TodoListAdapter (val todoList:ArrayList<Todo>, val adapterOnClick : (Any) -> Unit):RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
-    class TodoViewHolder(var view: View):RecyclerView.ViewHolder(view)
+    class TodoViewHolder(var view: TodoItemLayoutBinding):RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.todo_item_layout,parent,false)
+        val view = DataBindingUtil.inflate<TodoItemLayoutBinding>(inflater, R.layout.todo_item_layout,parent,false)
 
         return TodoViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.view.todo = todoList[position]
+        holder.view.checkTask.setText(todoList[position].title.toString())
 
-            holder.view.checkTask.setText(todoList[position].title.toString())
+        holder.view.imgEdit.setOnClickListener {
+            val action = TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
+            Navigation.findNavController(it).navigate(action)
+        }
 
-            holder.view.imgEdit.setOnClickListener {
-                val action = TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
-                Navigation.findNavController(it).navigate(action)
-            }
-
-            holder.view.checkTask.setOnCheckedChangeListener { compoundButton, isChecked ->
-                adapterOnClick(todoList[position])
-            }
+        holder.view.checkTask.setOnCheckedChangeListener { compoundButton, isChecked ->
+            adapterOnClick(todoList[position])
+        }
 
     }
 
